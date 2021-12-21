23d7b980f561398d0a038bb5e5de9a6357db5ea8
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2021-12-21 11:24:31 +0100

    locales folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import data from '../locales/numbers.json'
+
+const numbers = data;
 
 function App() {
   return (
@@ -7,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Phrase</h1>
+        {numbers.one}/{numbers.two}
       </header>
     </div>
   );
